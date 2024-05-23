6e1602626667be415645577cce226c064b3a21c1
Murksman <nicholasregehr@gmail.com>
2024-05-22 22:21:49 -0600

    Dev Alpha 2.5.2 - Refining tileSet manipulations and compilation methods.

--- Scripts/Map Scripts/MapGeneratorV3.gd
+++ Scripts/Map Scripts/MapGeneratorV3.gd
@@ -15,13 +15,16 @@ class_name MapGenerator
 	get: 
 		return false
 	set(arg):
-		TestFunc()
+		if tileMap != null:
+			TestFunc()
+
 @export var img_array : Array[Sprite2D]
 
 
 func GenerateImages():
 	for x in get_child_count():
 		get_child(get_child_count() - x - 1).queue_free()
+	
 	img_array = []
 	
 	var img : Image = src_image.get_image()
@@ -32,19 +35,18 @@ func GenerateImages():
 	
 	for y in partitions_col:
 		for x in partitions_row:
-			
 			var curr_img_pos = Vector2i(x,y) * partition_size
 			var new_img = img.get_region(Rect2i(curr_img_pos, Vector2i(partition_size, partition_size)))
 			
 			var new_sprite : Sprite2D = Sprite2D.new()
 			new_sprite.texture = ImageTexture.create_from_image(new_img)
 			$".".add_child(new_sprite)
-			new_sprite.position = Vector2(x,y) * partition_size
 			new_sprite.owner = get_tree().edited_scene_root
+			new_sprite.position = Vector2(x,y) * partition_size
 			new_sprite.name = "Sprite2D_" + str(x) + "_" + str(y)
 			img_array.append(new_sprite)
 
 
 func TestFunc():
-	if tileMap != null:
-		print("test")
+	tileMap.get_used_cells(0)
+	print("test")
