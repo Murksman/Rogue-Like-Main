f8e815fb4d5f0d38fdebea2e27a1579d93d8382a
Murksman <nicholasregehr@gmail.com>
2024-05-29 22:57:41 -0600

    Dev Alpha 2.7 - Reworked the light rendering of LOS system (switched out of viewports)

--- Scripts/Player Scripts/PlayerCam.gd
+++ Scripts/Player Scripts/PlayerCam.gd
@@ -1,12 +1,11 @@
 extends Camera2D
 
 var time : float = 0.0
-@export var visionPlane : SubViewport
 @export var mousePosCamMultiplier : float
 
 var direction
 
 func shader_orientation(dir):
-	offset = ($"../..".get_mouse_position() - Vector2(960, 540)) * mousePosCamMultiplier
-	$CanvasLayer.mouseShaderOffset = Vector2(offset.x / 540.0, offset.y / 540.0)
+	offset = (get_viewport().get_mouse_position() - Vector2(960, 540)) * mousePosCamMultiplier
+	#$CanvasLayer.mouseShaderOffset = Vector2(offset.x / 540.0, offset.y / 540.0)
 	direction = dir
--- Scripts/Player Scripts/UI Elements.gd
+++ Scripts/Player Scripts/UI Elements.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var player = $"../Main Plane Controller/Main Plane/Player"
+@onready var player = $".."
 @onready var healthText : Label = $"UI Bottom Left/Health Label"
 @onready var healthBar : TextureProgressBar = $"UI Bottom Left/Health Bar"
 @onready var ammoBar : ProgressBar = $"UI Bottom Right/Ammo Bar"
