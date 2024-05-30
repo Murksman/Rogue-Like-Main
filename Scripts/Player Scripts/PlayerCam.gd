d5b884da9edf09bc92445ddbc31259c2f412a72a
Nicholas Regehr <nicholas@inspiris.ca>
2024-05-30 16:58:46 -0600

    Dev Alpha 2.7.1 - Adjusting Lights and Shader behavior

--- Scripts/Player Scripts/PlayerCam.gd
+++ Scripts/Player Scripts/PlayerCam.gd
@@ -2,10 +2,10 @@ extends Camera2D
 
 var time : float = 0.0
 @export var mousePosCamMultiplier : float
+@onready var cone_shader : ColorRect = $"../Shader Container/Vision Cone Effects/Vision Cone Color Control"
 
-var direction
-
-func shader_orientation(dir):
+func shader_orientation(direction):
 	offset = (get_viewport().get_mouse_position() - Vector2(960, 540)) * mousePosCamMultiplier
+	cone_shader.material.set_shader_parameter("look_direction", direction)
+	cone_shader.material.set_shader_parameter("player_offset", Vector2(offset.x / 960, offset.y / 540))
 	#$CanvasLayer.mouseShaderOffset = Vector2(offset.x / 540.0, offset.y / 540.0)
-	direction = dir
--- Scripts/Player Scripts/VisionCam.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var cam = $"../../../Main Plane Controller/Main Plane/Player/Camera2D"
-
-func _process(delta):
-	global_position = cam.global_position
-	$"Player Dopple Cam".offset = cam.offset
