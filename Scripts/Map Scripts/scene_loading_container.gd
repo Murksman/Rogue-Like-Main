be5b1a4280b4c6b6c3e5d9d22124a160843a1678
Murksman <nicholasregehr@gmail.com>
2024-08-26 23:03:13 -0600

    Dev Alpha 3.39 - Made loading resources and fresh save functionality in the autoload script.

--- Scripts/Map Scripts/scene_loading_container.gd
+++ Scripts/Map Scripts/scene_loading_container.gd
@@ -1,5 +1,5 @@
 extends Node
-var load_file_path : String = "user://save1.tres"
+var load_file_path : String = ""
 var player_data : SaveData
 
 @onready var player = get_node("/root/World/Player")
@@ -8,19 +8,39 @@ var player_data : SaveData
 func _ready():
 	print("test")
 	if ResourceLoader.exists(load_file_path):
+		print("File Save Found at: \"" + load_file_path + "\"")
 		player_data = ResourceLoader.load(load_file_path)
 		print(player_data)
-	else:
-		player_data = SaveData.new()
-		print(player, " ", player_data)
+	#else:
+		#player_data = SaveData.new()
+		#print(player, " ", player_data)
 
-func SaveGame():
-	print("test")
+func SaveGame(file_path : String = load_file_path):
+	if !ResourceLoader.exists(load_file_path):
+		print("Creating new save...")
+		return FreshSave(file_path)
+	
 	var player_inventory = player.backpack_inventory
 	player_data.player_inventory_items.resize(player_inventory.inv_size.x * player_inventory.inv_size.y)
+	
 	for i in player_data.player_inventory_items.size():
 		var item = PackedScene.new()
 		item.pack(player_inventory.get_child(i).get_child(1))
 		player_data.player_inventory_items[i] = item
 	
-	ResourceSaver.save(player_data, load_file_path)
+	return ResourceSaver.save(player_data, load_file_path)
+
+func LoadGame(file_path : String):
+	if !ResourceLoader.exists(file_path):
+		return "File Not Found."
+	
+	player_data = ResourceLoader.load(file_path)
+	return player_data
+
+func FreshSave(file_path : String):
+	load_file_path = file_path
+	
+	player_data = SaveData.new()
+	print(ResourceSaver.save(player_data, load_file_path))
+	return ResourceSaver.save(player_data, load_file_path)
+	
--- Scripts/UI Scripts/FileLoadButton.gd
+++ Scripts/UI Scripts/FileLoadButton.gd
@@ -2,6 +2,16 @@ extends TextureButton
 
 @export var file_index : int
 
+@onready var save_container = $"../../../../.."
+
 func _pressed():
-	$"../../../../..".LoadFile(file_index).test_data
-	print("data: " + $"../../../../..".LoadFile(file_index).test_data)
+	var load_result = SceneLoadingContainer.LoadGame(save_container.save_paths[file_index])
+	if load_result is SaveData:
+		print("Successfully loaded data: " + load_result.test_data)
+		get_tree().change_scene_to_file("res://Scenes/Maps/Main.tscn")
+	else:
+		print(load_result)
+		var save_status = SceneLoadingContainer.SaveGame(save_container.save_paths[file_index])
+		save_container.save_slots[file_index] = (save_status == 0)
+		if (save_status == 0):
+			get_tree().change_scene_to_file("res://Scenes/Maps/Main.tscn")
