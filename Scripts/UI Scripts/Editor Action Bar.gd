5dc5430b25246084fb60ec711ddeafd04edebdc7
Murksman <nicholasregehr@gmail.com>
2024-11-13 22:28:59 -0700

    Dev Alpha 4.4.2 - Work on the Editor Buttons and Theming

--- Scripts/UI Scripts/Editor Action Bar.gd
+++ Scripts/UI Scripts/Editor Action Bar.gd
@@ -2,4 +2,4 @@ extends HSplitContainer
 
 func _on_dragged(offset: int) -> void:
 	var space : int = offset - 24
-	#$"Tile Tray/Tile Tray Container/Tray Scroller/Tray Grid".columns = floor(space / 86)
+	$"Tile Tray/Tile Tray Container/Tray Scroller/Tray Grid".columns = floor(space / 86)
